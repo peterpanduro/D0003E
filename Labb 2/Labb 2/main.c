819c64367d69b4535d4774942a095b2ea5354a90
Peter Panduro <larpet-5@student.ltu.se>
2019-02-07 13:39:14 +0100

    Created Lab 2

---
+++ Labb 2/Labb 2/main.c
@@ -0,0 +1,18 @@
+/*
+ * Labb 2.c
+ *
+ * Created: 2019-02-07 13:38:28
+ * Author : larpet-5
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
